bc3479837c6cdf0e4a530ed47bac891d0a503d43
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-08-25 20:17:51 +0200

    ✨ Implement  password hashing

--- Chattle/src/Model/Account.cs
+++ Chattle/src/Model/Account.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace Chattle
 {
@@ -7,31 +9,39 @@ namespace Chattle
         public Guid Id { get; private set; }
         public string Username { get; set; }
         public bool IsActive { get; set; }
-        public string PasswordHash { get; private set; }
-        public string PasswordSalt { get; private set; }
         public DateTime CreationTime { get; private set; }
+
+        private string passwordHash;
+        private string passwordSalt;
 
         public Account(string username, string password)
         {
             Id = Guid.NewGuid();
             Username = username;
             IsActive = true;
-            PasswordHash = String.Empty;
-            PasswordSalt = String.Empty;
+            passwordHash = String.Empty;
+            passwordSalt = String.Empty;
             ChangePassword(password);
             CreationTime = DateTime.UtcNow;
         }
 
         public void ChangePassword(string password)
         {
-            throw new NotImplementedException();
-            //TODO: Implement password hashing;
+            using var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, 16, 100000, HashAlgorithmName.SHA512);
+
+            passwordHash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(16));
+            passwordSalt = Convert.ToBase64String(rfc2898DeriveBytes.Salt);
         }
 
-        public void VerifyPassword(string passwordToVerify)
+        public bool VerifyPassword(string passwordToVerify)
         {
-            throw new NotImplementedException();
-            //TODO: Implement password verification;
+            var salt = Convert.FromBase64String(passwordSalt);
+            var hash = Convert.FromBase64String(passwordHash);
+
+            using var rfc2898DeriveBytes = new Rfc2898DeriveBytes(passwordToVerify, salt, 100000, HashAlgorithmName.SHA512);
+
+            var newHash = rfc2898DeriveBytes.GetBytes(16);
+            return newHash.SequenceEqual(hash);
         }
     }
 }
