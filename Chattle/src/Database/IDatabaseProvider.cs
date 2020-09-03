ea35319b9410463d84c6022dfec02b801c9a93e1
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-09-03 10:10:30 +0200

    ✨ Create `IDatabaseProvider` interface

---
+++ Chattle/src/Database/IDatabaseProvider.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+
+namespace Chattle.Database
+{
+    public interface IDatabaseProvider
+    {
+        public void Create<T>(string collectionName, T item) where T : IIdentifiable;
+
+        public List<T> Read<T>(string collectionName, Expression<Func<T, bool>> expression, int limit = 0) where T : IIdentifiable;
+
+        public void Update<T>(string collectionName, Guid id, T newItem) where T : IIdentifiable;
+
+        public void Delete<T>(string collectionName, Guid id) where T : IIdentifiable;
+    }
+}
