ed273930aa05b6b4f59f02576bd0cea7e5315f03
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-09-06 21:29:50 +0200

    ✨ Implement Defaul Image for `User` and `Server`

--- Chattle/src/DefaultImage.cs
+++ Chattle/src/DefaultImage.cs
@@ -1,20 +1,20 @@
 using System;
 
-namespace Chattle.Helpers
+namespace Chattle
 {
     public static class DefaultImage
     {
-        private static readonly Uri baseUserImageUri = new Uri("https://api.adorable.io/avatars/512/");
-        private static readonly Uri baseChannelImageUri = new Uri("https://api.adorable.io/avatars/512/");
+        private static readonly Uri baseUserImageUri = new Uri("https://api.adorable.io/avatars/512/User_");
+        private static readonly Uri baseServerImageUri = new Uri("https://api.adorable.io/avatars/512/Server_");
 
-        public static Uri GetUserImage(string username)
+        public static Uri GetUserImage(Guid userId)
         {
-            return new Uri(baseUserImageUri, username);
+            return new Uri(baseUserImageUri, userId.ToString());
         }
 
-        public static Uri GetChannelImage(string name)
+        public static Uri GetServerImage(Guid serverId)
         {
-            return new Uri(baseChannelImageUri, name);
+            return new Uri(baseServerImageUri, serverId.ToString());
         }
     }
 }
