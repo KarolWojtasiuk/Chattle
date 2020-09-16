212fcaeade2d9617c0515a8963f4ba804d01bda9
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-09-16 11:05:47 +0200

    ✨ Add `ServerId` to `Channel`

--- Chattle/src/Model/Channel.cs
+++ Chattle/src/Model/Channel.cs
@@ -7,24 +7,27 @@ namespace Chattle
         public Guid Id { get; private set; }
         public string Name { get; internal set; }
         public string Description { get; internal set; }
-        public Guid ChannelId { get; private set; }
+        public Guid ServerId { get; private set; }
+        public Guid AuthorId { get; private set; }
         public DateTime CreationTime { get; private set; }
 
-        public Channel(string name, Guid authorId, string description)
+        public Channel(string name, Guid serverId, Guid authorId, string description)
         {
             Id = Guid.NewGuid();
             Name = name;
             Description = description;
-            ChannelId = authorId;
+            ServerId = serverId;
+            AuthorId = authorId;
             CreationTime = DateTime.UtcNow;
         }
 
-        public Channel(string name, Guid authorId)
+        public Channel(string name, Guid serverId, Guid authorId)
         {
             Id = Guid.NewGuid();
             Name = name;
             Description = String.Empty;
-            ChannelId = authorId;
+            ServerId = serverId;
+            AuthorId = authorId;
             CreationTime = DateTime.UtcNow;
         }
 
