055f5497fad00a6a44f682058161a2b0a698358d
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-10-01 20:05:38 +0200

    🐛 Fix typo

--- Chattle.SignalR/Program.cs
+++ Chattle.SignalR/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Chattle.SingalR
+namespace Chattle.SignalR
 {
     public class Program
     {
