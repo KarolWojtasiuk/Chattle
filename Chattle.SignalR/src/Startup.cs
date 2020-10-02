40597a4ac0b19b92dd27a3331e4d41a5331daa0d
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-10-02 22:54:00 +0200

    🚚 Move source files to `src` directory

--- Chattle.SignalR/src/Startup.cs
+++ Chattle.SignalR/src/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,7 +9,7 @@ namespace Chattle.SignalR
     {
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSignalR();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -28,10 +23,6 @@ namespace Chattle.SignalR
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
             });
         }
     }
