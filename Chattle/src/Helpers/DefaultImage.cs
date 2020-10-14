919d303d1910795955465299d25ebaf3b4516be7
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-10-14 15:32:58 +0200

    🚑 Fix `DefaultImage`

--- Chattle/src/Helpers/DefaultImage.cs
+++ Chattle/src/Helpers/DefaultImage.cs
@@ -4,17 +4,20 @@ namespace Chattle
 {
     public static class DefaultImage
     {
-        private static readonly Uri baseUserImageUri = new Uri("https://api.adorable.io/avatars/512/User_");
-        private static readonly Uri baseServerImageUri = new Uri("https://api.adorable.io/avatars/512/Server_");
+        //! Find working API;
+        // private static readonly Uri baseUserImageUri = new Uri("https://api.adorable.io/avatars/512/User_");
+        // private static readonly Uri baseServerImageUri = new Uri("https://api.adorable.io/avatars/512/Server_");
 
         public static Uri GetUserImage(Guid userId)
         {
-            return new Uri(baseUserImageUri, userId.ToString());
+            // return new Uri(baseUserImageUri, userId.ToString());
+            return new Uri("http://placehold.jp/500x500.jpg");
         }
 
         public static Uri GetServerImage(Guid serverId)
         {
-            return new Uri(baseServerImageUri, serverId.ToString());
+            return new Uri("http://placehold.jp/500x500.jpg");
+            // return new Uri(baseServerImageUri, serverId.ToString());
         }
     }
 }
