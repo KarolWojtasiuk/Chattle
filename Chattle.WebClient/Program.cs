e18eb644bf6cfdd651934be78725606521fb80ab
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-10-30 19:27:20 +0100

    ➕ Add Blazorise

--- Chattle.WebClient/Program.cs
+++ Chattle.WebClient/Program.cs
@@ -3,6 +3,9 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Blazorise;
+using Blazorise.Bulma;
+using Blazorise.Icons.FontAwesome;
 
 namespace Chattle.WebClient
 {
@@ -11,11 +14,18 @@ namespace Chattle.WebClient
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
             builder.RootComponents.Add<App>("#app");
-
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazorise();
+            builder.Services.AddBulmaProviders();
+            builder.Services.AddFontAwesomeIcons();
 
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+            host.Services.UseBulmaProviders();
+            host.Services.UseFontAwesomeIcons();
+
+            await host.RunAsync();
         }
     }
 }
