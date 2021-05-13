a88a3db377b1de95ac2011febfb337a6b24ed0d5
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2021-05-13 17:03:16 +0200

    feat(database): Add MessageEntity

---
+++ Chattle/Database/Entities/Message.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Chattle.Database.Entities
+{
+    public record MessageEntity
+    {
+        public Guid Id { get; private init; }
+        public string Content { get; init; }
+        public Guid ChannelId { get; private init; }
+        public Guid AuthorId { get; private init; }
+        public DateTime CreationDate { get; private init; }
+
+        public MessageEntity(string content, Guid channelId, Guid authorId)
+        {
+            Id = Guid.NewGuid();
+            Content = content;
+            ChannelId = channelId;
+            AuthorId = authorId;
+            CreationDate = DateTime.UtcNow;
+        }
+    }
+}
