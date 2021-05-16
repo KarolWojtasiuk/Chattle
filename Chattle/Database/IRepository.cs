166566adb89e07d2d27822beea2099bf260c8a0c
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2021-05-16 15:41:43 +0200

    feat(database): Remove Update method

--- Chattle/Database/IRepository.cs
+++ Chattle/Database/IRepository.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using Chattle.Database.Entities;
 
-namespace Chattle.Database.Repositories
+namespace Chattle.Database
 {
     public interface IRepository<T> where T : IEntity
     {
         public void Insert(T item);
         public void Replace(T item);
-        public void Update<TProperty>(Guid id, Expression<Func<T, TProperty>> expression, TProperty value);
         public void Delete(Guid id);
         public T Get(Guid id);
         public T FindOne(Expression<Func<T, bool>> expression);
--- Chattle/Database/Repository.cs
+++ Chattle/Database/Repository.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using Chattle.Database.Entities;
 
-namespace Chattle.Database.Repositories
+namespace Chattle.Database
 {
     public abstract class Repository<T> : IRepository<T> where T : IEntity
     {
-        private readonly IDatabaseContext _databaseContext;
-        private readonly string _collectionName;
-
         protected Repository(IDatabaseContext databaseContext, string collectionName)
         {
             _databaseContext = databaseContext;
             _collectionName = collectionName;
         }
+
+        private readonly IDatabaseContext _databaseContext;
+        private readonly string _collectionName;
 
         public void Insert(T item)
         {
@@ -24,11 +24,6 @@ namespace Chattle.Database.Repositories
         public void Replace(T item)
         {
             _databaseContext.Replace(item, _collectionName);
-        }
-
-        public void Update<TProperty>(Guid id, Expression<Func<T, TProperty>> expression, TProperty value)
-        {
-            _databaseContext.Update(id, expression, value, _collectionName);
         }
 
         public void Delete(Guid id)
